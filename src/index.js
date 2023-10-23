7cd94144c6b49a9f9d3329870961bf62858d6beb
aviv-taori <134504658+aviv-taori@users.noreply.github.com>
2023-10-23 21:53:37 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import logoReact from './logoReact.png';
+import "./index.css"
+
+function Header() {
+  return (
+   <header>
+    <nav className="nav">
+      <img src={logoReact} width={60} />
+
+      <ul className="nav-items">
+        <li>
+          1
+        </li>
+        <li>
+          2
+        </li>
+        <li>
+          3
+        </li>
+      </ul>
+    </nav>
+   </header>
+  )
+}
+
+function Content() {
+  return (
+    <div>
+      <h1>
+        This is first page
+      </h1>
+      <ul>
+        <li>
+          4
+        </li>
+        <li>
+          5
+        </li>
+        <li>
+          6
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+    <small>
+      Don't forget to smile :)
+    </small>
+    </footer>
+  )
+}
+
+function Page() {
+  return (
+    <div>
+    <Header/>
+    <Content/>
+    <Footer/>
+    </div>
+
+
+  )
+}
+
+// const page = Page()
+// console.log(page)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Page/>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
