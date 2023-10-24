0a9f1294d83faaf67090952abfb99f8b5d71631e
aviv-taori <134504658+aviv-taori@users.noreply.github.com>
2023-10-24 10:50:23 +0300

    fourth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-import logo_React from './logoReact.png';
+import logo_React from '../images/logoReact.png';
 
 export default function Header() {
     return (
      <header>
       <nav className="nav">
-        <img className="logo" src={logo_React}/>
+        <img className="logo" alt='logo' src={logo_React}/>
   
         <ul className="nav-items">
           <li> 1</li>
--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 import ReactDOM from 'react-dom/client';
 
-import Header from './components/Header';
-import Content from './components/Content';
-import Footer from './components/Footer';
-import "./index.css"
-
-
-function Page() {
-  return (
-    <div>
-    <Header/>
-    <Content/>
-    <Footer/>
-    </div>
-
-
-  )
-}
-
-// const page = Page()
-// console.log(page)
+import "./index.css";
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Page/>);
-
+root.render(<App/>);
